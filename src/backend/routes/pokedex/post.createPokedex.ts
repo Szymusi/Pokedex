496fd81330a31370fb403fae8a61c88b3534731d
Szymon Danielak <danielak.szymon@gmail.com>
2023-06-10 08:57:21 +0200

    Changing createPokedex

--- src/backend/routes/pokedex/post.createPokedex.ts
+++ src/backend/routes/pokedex/post.createPokedex.ts
@@ -7,16 +7,8 @@ import { checkPrismaError } from '../../utils'
 export const NewDeck: RequestHandler = async (req, res) => {
     const pokedexService = new PokedexService()
     const { email, name } = req.body
-
-    try {
-        var resp = await pokedexService.CreateNewDeck(email,name)
-        res.status(resp.statusCode)
-        res.send({pokedexId: resp.message})
-    } catch (err) {
-        const response = checkPrismaError(err, {
-        uniqueConstraintFailed: 'Email must be unique.',
-        })
-        res.status(response.status)
-        res.send(response.message)
-    }
+    
+    var resp = await pokedexService.CreateNewDeck(email,name)
+    res.status(resp.statusCode)
+    res.send({pokedexId: resp.message})    
 }
